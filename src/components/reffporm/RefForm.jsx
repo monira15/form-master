fa9df590ed66cf21323a83e1d3a0b1a47ed75122
monira15 <monira.programmer@gmail.com>
2023-09-21 17:16:06 +0600

    first commit

--- src/components/reffporm/RefForm.jsx
+++ src/components/reffporm/RefForm.jsx
@@ -1,16 +1,23 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 const RefForm = () => {
     const nameref =useRef(null)
     const emailref =useRef(null)
     const passwordref=useRef(null)
+    const [password,setpassword]=useState(null)
     const handlesubmit= e =>{
        
         e.preventDefault()
+        if(passwordref.current.value.length < 6){
+            setpassword('password must be 6 characters')
+        }else{
+            setpassword('')
+        }
         console.log(nameref.current.value);
         console.log(emailref.current.value);
         console.log(passwordref.current.value);
+        console.log(password);
     }
     useEffect(()=>{
         nameref.current.focus();
@@ -22,12 +29,15 @@ const RefForm = () => {
            <form onSubmit={handlesubmit} >
             <input ref={nameref} type="text" name="name" className=" bg-gray-300 mt-4 py-2 p-2"/>
             <br />
-            <input ref={emailref} type="email"  name="email" className=" bg-gray-300 mt-4 py-2 p-2" />
+            <input ref={emailref} defaultValue={'monira.programmer@gmail.com'} type="email"  name="email" className=" bg-gray-300 mt-4 py-2 p-2" />
             <br />
             <input ref={passwordref} type="password" name="password"className=" bg-gray-300 mt-4 py-2 p-2"/>
             <br />
 
             <input type="submit" value="submit" />
+            {
+                password && <p>{password}</p>
+            }
         </form>  
         </div>
     );
